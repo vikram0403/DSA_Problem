fbd2fdfb47c9b34b03438dbb45eb7f28338123c3
vikramprakash <vikramprakash1015@gmail.com>
2025-05-24 13:08:44 +0530

    largestElements

---
+++ src/main/java/Largest_Element.java
@@ -0,0 +1,20 @@
+public class Largest_Element {
+    public static int largestElements(int arr[]){
+        int largest = Integer.MIN_VALUE;
+        for(int i =0; i<arr.length;i++){
+            if (largest<arr[i]){
+                largest=arr[i];
+            }
+
+        }
+        return largest;
+    }
+
+    public static void main(String args[]){
+        int arr[]={3, 3, 0, 99, -40};
+        int large=largestElements(arr);
+        System.out.println("largest Elements:"+large);
+    }
+
+
+}
